6c0ca484e6eb1c92306c00d813f9d829c58d1332
d0now <dolpin1402@gmail.com>
2019-10-18 06:22:45 -0700

    Binder protocol added

---
+++ super_modern_service/main.c
@@ -0,0 +1,12 @@
+/*
+ * Super modern service by d0now
+ * for belluminar 2019
+ * git : github.com/d0now/2019_belluminar
+*/
+
+#include <stdio.h>
+#include "binder.h"
+
+int main(void) {
+	return 0;
+}
